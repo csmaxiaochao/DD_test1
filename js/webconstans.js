02fc069365500552e68e98c65adfdc3b9c0e4e34
qc_0a9b4d8ce16748f79f1074452c1b6315 <qc_0a9b4d8ce16748f79f1074452c1b6315@git.code.tencent.com>
2018-12-17 09:46:42 +0800

    create respository

---
+++ js/webconstans.js
@@ -0,0 +1,23 @@
+
+//---------------------------一带一路界面--------------------------------
+var HOST = 'http://192.168.0.146:8001/Api/';
+var PATH = 'https://api.jizhibao.com.cn/';
+var BASEPATH = 'http://lzz.jizhibao.com.cn/';
+//获取地域信息
+var GET_REGION_INFO = BASEPATH+'RoadInspectOrder/GetAreaTreeAll';
+//一路一带条目
+var GET_LOAD_LIST = BASEPATH+'RoadInspectOrder/GetRoadList';
+//一路一带详情接口
+var GET_LOAD_DETAIL = BASEPATH+'RoadInspectOrder/GetRoadDetails';
+
+//以项目代替区域
+var GET_PROJ_CONT_LIST_URL = BASEPATH +'Project/GetUserProContract';
+
+//---------------------------新闻详情界面-------------------------------
+////获取新闻详情界面
+//var GET_NEWS_DETAILINFO = HOST + 'News/GetNewsDetail.ashx';
+////获取新闻更多列表
+//var GET_NEWS_LISTINFO = HOST +'Account/GetAllNews.ashx';
+var GET_NEWS_LISTINFO='https://website.jizhibao.com.cn/News/getmynewslist';
+
+
